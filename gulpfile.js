1ae7e48b772735fb58c9682e6d6b3d28628457ee
Dauren_Altynbekov <Dauren_Altynbekov@epam.com>
2015-07-03 16:26:08 +0600

    all html and js files minified and concatineted

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var concat = require('gulp-concat');
+var bower = require('gulp-bower');
+
+gulp.task('default', ['minHtml','scripts','bower']);
+
+gulp.task('minHtml',function() {
+  return gulp.src('public/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(['./public/**/*.module.js','./public/**/*.controller.js','./public/**/*.js'])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest('./dist/components/'))
+});
